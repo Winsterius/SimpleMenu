ac0f39f53586889ef6709ccacac906195ab45d92
Juri Schreiner <Juri.Schreiner@drheuer.onmicrosoft.com>
2019-04-24 19:15:03 +0200

    Deleted to test

--- SimpleMenu/Program.cs
+++ SimpleMenu/Program.cs
@@ -17,7 +17,6 @@ namespace SimpleMenu
                 Console.Clear();
                 Console.WriteLine("S -> Summe berechnen \nF -> Fakultät berechnen \nG -> Größten gemeinsamen Teiler berechnen \nM -> Meilen berechnen \nE -> Programm beenden");
                 menu.ShowMenu(Console.ReadLine());
-                Console.ReadKey();
             }
         }                    
     }
